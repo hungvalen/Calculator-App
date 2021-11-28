a6463a6602e1f6d4e48090bb984c3a4794ff32ba
hungvalen <pham50111@gmail.com>
2021-11-28 17:06:00 +0700

    react calculator initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Button from './components/Button';
+import ButtonBox from './components/ButtonBox';
+import Screen from './components/Screen';
+import Wrapper from './components/Wrapper';
 
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, 'X'],
+  [4, 5, 6, '-'],
+  [1, 2, 3, '+'],
+  [0, '.', '=']
+]
 function App() {
+  const [calc,setCalc] = useState({
+    sign:"",
+    num:0,
+    res:0
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+        {
+          btnValues.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                className={btn === "=" ? "equals" : ""}
+                value={btn}
+                onClick={() => {
+                  // console.log(`${btn} clicked`)
+                  btn === "C" 
+                  ? resetClickHandler 
+                  : btn === "+-"
+                  ? invertClickHandler
+                  : btn === "%"
+                  ? percentClickHandler
+                  : btn === "="
+                  ? equalsClickHandler
+                  : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+                  ? operatorClickHandler
+                  : btn === "."
+                  ? commaClickHandler
+                  : numClickHandler
+                }}
+              >
+
+              </Button>
+            )
+          })
+        }
+      </ButtonBox>
+    </Wrapper>
   );
 }
 
